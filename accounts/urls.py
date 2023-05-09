0b4fdc15264f504e39cecdec50ef3143535cc435
Qiunn <qiunn@MacBook-Pro-cua-Qiunn.local>
2023-05-09 13:59:02 +0700

    quinn: redirect login page when do not login

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    path('', (views.homepage_after_login), name="homepage_after_login"),
+    path('', login_required(views.homepage_after_login), name="homepage_after_login"),
     path('login/', views.loginCustom, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('password-change/', login_required(views.password_change, login_url='login'), name='changePassword'),
